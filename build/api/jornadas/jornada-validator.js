97f128296680e2788839c997c5eeba34f0f55189
TomasReisenman <tom.manrey@gmail.com>
2019-11-02 23:40:31 -0300

    removed build from repo

--- build/api/jornadas/jornada-validator.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Joi = require("joi");
-exports.jwtValidator = Joi.object({ authorization: Joi.string().required() }).unknown();
-//# sourceMappingURL=jornada-validator.js.map
--- build/database/actividades_voluntarios.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_1 = require("sequelize");
-class ActividadesVoluntarios extends sequelize_1.Model {
-}
-exports.ActividadesVoluntarios = ActividadesVoluntarios;
-//# sourceMappingURL=actividades_voluntarios.js.map
